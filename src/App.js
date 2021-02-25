dc5fcac89f74e49177334563df29831bdb8fc8ae
andredonati <47343046+andredonati@users.noreply.github.com>
2021-02-25 15:59:28 -0600

    adding in google maps for ios

--- src/App.js
+++ src/App.js
@@ -7,29 +7,12 @@
  */
 
 import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import AppleMaps from './screens/MapView.js';
 
-class AppComponent extends Component{
-  render(){
-    return (
-      <AppleMaps></AppleMaps>
-    );
+class AppComponent extends Component {
+  render() {
+    return <AppleMaps />;
   }
 }
 
--- src/screens/MapView.js
+++ src/screens/MapView.js
@@ -1,22 +1,31 @@
 import React, {Component} from 'react';
-import MapView from 'react-native-maps';
-
-
-
-export default class AppleMaps extends Component {  
-    render() {    
-        return (      
-        <MapView        
-            style={{flex: 1}}        
-            region={{          
-                latitude: 42.882004,          
-                longitude: 74.582748,          
-                latitudeDelta: 0.0922,          
-                longitudeDelta: 0.0421        
-            }}        
-            showsUserLocation={true}      
-        />    
-        );  
-    }
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, View} from 'react-native';
+export default class AppleMaps extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+        />
+      </View>
+    );
+  }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
