55335bbd1c24d809a0cfe7607d7a6f826c74c3e7
andredonati <47343046+andredonati@users.noreply.github.com>
2021-04-01 14:38:26 -0500

    adding in curernt location

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,26 +1,57 @@
 import React from 'react';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, Alert} from 'react-native';
+import Geolocation from 'react-native-geolocation-service';
 
-export default function Home() {
-  console.log('HELLOOOOOO');
-  return (
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          // eslint-disable-next-line prettier/prettier
-          latitude: 36.053100,
-          // eslint-disable-next-line prettier/prettier
-          longitude: -94.183800,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        showsUserLocation={true}
-      />
-    </View>
-  );
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+      coordinates: [],
+    };
+  }
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          coordinates: this.state.coordinates.concat({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          }),
+        });
+      },
+      (error) => {
+        Alert.alert(error.message.toString());
+      },
+      {
+        showLocationDialog: true,
+        enableHighAccuracy: true,
+        timeout: 20000,
+        maximumAge: 0,
+      },
+    );
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          provider={PROVIDER_GOOGLE}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+          showsUserLocation={true}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
